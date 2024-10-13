117a5ca2b0f7815bdcac3745878352bfea697c9a
Damian Satterthwaite-Phillips <damiansp@gmail.com>
2024-10-12 17:24:03 -0700

    rm obsolete code

--- strength_training/app/updating.py
+++ strength_training/app/updating.py
@@ -6,12 +6,3 @@ class Updater:
         data = pd.read_csv(inpath)
         data.training_max += data.increment_per_cycle
         data.to_csv(inpath, index=False)
-
-    @staticmethod
-    def _update_day(day):
-        for ex_type in ['main', 'support']:
-            if ex_type  in day:
-                day[ex_type] = [
-                    (exercise, tr_max + incr, incr)
-                    for (exercise, tr_max, incr) in day[ex_type]]
-        return day
